761c03ad55e395f1cd8b19e6768f1e5fba2d48f5
An Kieu <134972544+KoisenAn@users.noreply.github.com>
2026-03-27 11:13:12 -0400

    More work on model

--- smog_tower_simulation.py
+++ smog_tower_simulation.py
@@ -3,14 +3,18 @@ import numpy as np
 from scipy.integrate import odeint, solve_ivp
 import matplotlib.pyplot as plt
 
+GRAVITY = 9.81 # m/s^2
+VACUUM_PERMITTIVITY = 8.854 * 10e-12 # C^2/(N•m^2)
+
 AIR_DENSITY = 1.225 # kg/m^3 (25C)
 AIR_VISCOSITY = 18.6 * 10e-6 # Pa•s (25C)
-GRAVITY = 9.81 # m/s^2
+FORM_CONSTANT = 24
 
-CHARGE_OF_TOWER = 3 # Coulombs
+TOWER_CHARGE_DENSITY = 10e-4 # C
 
-particle_density = 1 * 10e-6 # m
+particle_density = 2.8 # kg / m^3
 particle_diameter = 1 * 10e-6 # m
+particle_charge = 1 * 10e-6 # C
 
 particle_position = [0,0] 
 particle_velocity = [0,0] # m/s
@@ -20,10 +24,21 @@ def system_of_equations(Y, t):
 
     x, y = Y
     
-    force_gravity_y = math.pi / 6 * particle_density * GRAVITY * (particle_diameter ** 3)
+    velocity_magnitude = (particle_velocity[0] ** 2 + particle_velocity[1] ** 2) ** (0.5)
+
+    force_gravity_y = -(math.pi / 6 * particle_density * GRAVITY * (particle_diameter ** 3))
     force_buoyancy_y = math.pi / 6 * AIR_DENSITY * GRAVITY * (particle_diameter ** 3)
 
-    drag_force_magnitude = 0.5 * 
+    reynolds_number = AIR_DENSITY * velocity_magnitude / AIR_VISCOSITY
+    form_drag = FORM_CONSTANT / reynolds_number
+
+    drag_force_magnitude = 0.5 * AIR_DENSITY * form_drag * (math.pi / 4 * particle_density ** 2) * velocity_magnitude ** 2
+    drag_force_x = - particle_velocity[0] / velocity_magnitude * drag_force_magnitude
+    drag_force_y = - particle_velocity[1] / velocity_magnitude * drag_force_magnitude
+
+    electric_field_plate_force_x = particle_charge * TOWER_CHARGE_DENSITY / (2 * VACUUM_PERMITTIVITY)
+
+    #electric_field_cloud_force_x = 
 
     dxdt = x
     dxdt = -2 * x - y
